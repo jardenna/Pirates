c90c783ffb1be31c73758898a3fa16c57e92250f
jardenna <hbjornum@gmail.com>
2021-11-20 11:43:53 +0100

    Added new js

---
+++ javascript/innerText.js
@@ -0,0 +1,7 @@
+// nav.innerHTML = `   
+// <ul>
+//     <li class="nav-item"> <a href="index.html">Hjem</a></li>
+//     <li class="nav-item"> <a href="profil.html">Profil</a></li>
+//     <li class="nav-item"> <a href="kontakt.html">Kontakt</a></li>
+// </ul>
+// `;
---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+//Get HTML element ved at bruge id
+
+const nav = document.getElementById("mainNav");
+
+const imgContainer = document.querySelector(".image")
+
+const para = document.querySelectorAll(".para")
+console.log(para, imgContainer);
+
+console.log(1 + 2);
+console.log(2+ "d")
