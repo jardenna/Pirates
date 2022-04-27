9003af39271e9acec1dc4893db64c5bcddf8c80a
jardenna <hbjornum@gmail.com>
2022-04-27 21:15:11 +0200

    Start

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-//Get HTML element ved at bruge id
-
-// const nav = document.getElementById("mainNav");
-
-// const imgContainer = document.querySelector(".image");
-
-// const para = document.querySelectorAll(".para");
-
-//console.log(para, nav, imgContainer);
